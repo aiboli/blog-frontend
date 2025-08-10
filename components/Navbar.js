83e954b4857cb84fb5325732144e992294c822fd
aiboli <albertlabtech@gmail.com>
2025-08-09 19:10:57 -0700

    update works page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,24 @@
-import { Nav } from 'react-bootstrap';
-import styles from '../styles/Navbar.module.css';
+import { Nav } from "react-bootstrap";
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
-    return (
-        <Nav
-            activeKey="/"
-            className={styles.myNav}
-        >
-            <Nav.Item>
-                <Nav.Link href="/"><h5>Home</h5></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/articles" eventKey="link-1"><h5>Articles</h5></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="/contact" eventKey="link-2"><h5>Contact</h5></Nav.Link>
-            </Nav.Item>
-        </Nav>
-    )
+  return (
+    <Nav activeKey="/" className={styles.myNav}>
+      <Nav.Item>
+        <Nav.Link href="/">
+          <h5>Home</h5>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/articles" eventKey="link-1">
+          <h5>Articles</h5>
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/works" eventKey="link-2">
+          <h5>Works</h5>
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
 }
